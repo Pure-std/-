0aa9df3f78482b928dc0d4a06218365f1e3c8c85
Pure-std <tukan222203@gmail.com>
2025-02-17 15:10:35 +0300

    Added new function sort_by_date.

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,22 @@
 def filter_by_state(operations: list, state = "EXECUTED") -> list:
-    """"принимает список словарей и опционально значение для ключа state
-        (по умолчанию 'EXECUTED').
-        Функция возвращает новый список словарей, содержащий только те словари,
-        у которых ключ stateсоответствует указанному значению."""
+    """"
+    Принимает список словарей и опционально значение для ключа state
+    (по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей, содержащий только те словари,
+    у которых ключ stateсоответствует указанному значению.
+    """
     filtered_list = []
     for x in operations :
         if x["state"] == state :
             filtered_list.append(x)
     return filtered_list
+
+def sort_by_date(operations: list, reverse_parameter = True) -> list:
+    """
+    Принимает список словарей и необязательный параметр,
+    задающий порядок сортировки (по умолчанию — убывание).
+    Функция должна возвращать новый список, отсортированный по дате
+    (date).
+    """
+    return sorted(operations, key=lambda d: d['date'], reverse= reverse_parameter)
+
