9a98a60141a001544383ca288e69c72a67232b42
Volkov.A <tukan222203@gmail.com>
2025-03-26 12:04:20 +0300

    added logging for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,17 @@
 import json
+import logging
+from pathlib import Path
 from typing import Any
+
+filedir = Path.cwd()
+logger = logging.getLogger("utils.py")
+# Основная конфигурация logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    filename=filedir.parent / "logs/utils.py.log",  # Запись логов в файл
+    filemode="a",
+)  # Перезапись файла при каждом запуске
 
 
 def read_json_data(filepath: str) -> Any:
@@ -10,10 +22,12 @@ def read_json_data(filepath: str) -> Any:
     :param filepath:
     :return:
     """
+    logger.info(f"read_json_data: input({filepath})")
     try:
         with open(filepath, "r", encoding="UTF-8") as file:
             data = json.load(file)
     except Exception as e:
-        print(f"Exception at utils.py: {e}")
+        logger.error(f"read_json_data: {e}")
         data = []
+    logger.info(f"read_json_data: output({data})")
     return data
