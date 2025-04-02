87950d5aaa2e10c810687372465beb78089cee67
Pure-std <tukan222203@gmail.com>
2025-04-02 15:28:41 +0300

    Docstrings.

--- src/read.py
+++ src/read.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 def read_csv(filepath: str) -> list[dict]:
+    """Функция для чтения csv файлов. Принимает путь до файла относительно родительской директории"""
     filedir = Path.cwd()
     with open(filedir.parent / filepath, encoding="utf-8") as file:
         reader = csv.DictReader(file, delimiter=";")
@@ -12,6 +13,7 @@ def read_csv(filepath: str) -> list[dict]:
 
 
 def xlsx_read(filepath: str) -> list[dict]:
+    """Функция для чтения xlsx файлов. Принимает путь до файла относительно родительской директории"""
     filedir = Path.cwd()
     reader = pd.read_excel(filedir.parent / filepath)
     return reader.to_dict("records")
